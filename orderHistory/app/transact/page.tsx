b83fdf8ee066c18dc570767baabd9fea1270d94f
sachinprasanna7 <sachincodes1@gmail.com>
2025-08-06 16:36:40 +0530

    Removed old folders to start fresh

--- orderHistory/app/transact/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TransactPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-semibold text-gray-900">Transact</h1>
-      <p className="mt-4 text-gray-600">Transaction content coming soon...</p>
-    </div>
-  )
-}
--- orderHistory/components/order-history/order-history-header.tsx
+++
@@ -1,7 +0,0 @@
-export function OrderHistoryHeader() {
-  return (
-    <div className="border-b border-gray-200 pb-4">
-      <h1 className="text-2xl font-semibold text-gray-900">Order History</h1>
-    </div>
-  )
-}
